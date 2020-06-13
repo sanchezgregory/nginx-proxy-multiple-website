9c2aaa9e0c6c50ee5c4490ef48ae8e8fe3724cc8
Gregory Sanchez <gsanchezaper@gmail.com>
2020-06-13 17:59:53 -0300

    se agrega nuevo proyecto, store2 y se conecta al mismo contenedor mysqldb

--- store1/index.php
+++ store1/index.php
@@ -1,2 +1,25 @@
 <?php
-echo phpinfo();
+$servername = "mysqldb";
+$username = "root";
+$password = "root";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=icbc", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+};
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		ICBC STORE
+	</title>
+</head>
+<body>
+	<H2>ICBC working</H2>
+</body>
+</html>
